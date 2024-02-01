2c7ae1115bd58a9e71398e09977410d7a1f17d32
Jiří Hofman <jiri.hofman@gmail.com>
2024-02-01 13:54:31 +0100

    Commented out network resources and used existing VPC, subnet, and security group

--- main.tf
+++ main.tf
@@ -7,83 +7,95 @@ variable "subdomain" {
   type        = string
 }
 
-data "aws_instances" "existing" {
-  instance_tags = {
-    Name = "jirka-RT-${var.subdomain}"
+# The network stuff already exists, so we can comment it out
+# We will use the existing VPC, subnet and security group
+
+# resource "aws_vpc" "demo_vpc" {
+#   cidr_block = "10.0.0.0/16" # Replace with your desired CIDR block
+
+#   enable_dns_hostnames = true
+#   enable_dns_support   = true
+
+#   tags = {
+#     Name = "jirka-VPC"
+#   }
+# }
+
+# resource "aws_internet_gateway" "gw" {
+#   vpc_id = aws_vpc.demo_vpc.id
+# }
+
+# resource "aws_route_table" "route_table" {
+#   vpc_id = aws_vpc.demo_vpc.id
+
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.gw.id
+#   }
+#   tags = {
+#     Name = "jirka-RT"
+#   }
+# }
+
+# resource "aws_subnet" "my_subnet" {
+#   vpc_id            = aws_vpc.demo_vpc.id
+#   cidr_block        = "10.0.1.0/24" # Replace with your desired CIDR block
+#   availability_zone = "eu-central-1a"
+#   tags = {
+#     Name = "jirka-subnet"
+#   }
+# }
+
+# resource "aws_route_table_association" "route_table_asso" {
+#   subnet_id      = aws_subnet.my_subnet.id
+#   route_table_id = aws_route_table.route_table.id
+# }
+
+# resource "aws_security_group" "instance_sg" {
+#   name        = "jirka_webserver_sg2"
+#   description = "Allow inbound SSH and HTTP traffic"
+#   vpc_id      = aws_vpc.demo_vpc.id
+
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   ingress {
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   tags = {
+#     Name = "jirka-web-traffic"
+#   }
+# }
+
+data "aws_subnet" "existing" {
+  filter {
+    name   = "tag:Name"
+    values = ["jirka-subnet"]
   }
 }
 
-resource "aws_vpc" "demo_vpc" {
-  cidr_block = "10.0.0.0/16" # Replace with your desired CIDR block
-
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  tags = {
-    Name = "jirka-VPC"
-  }
-}
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.demo_vpc.id
-}
-
-resource "aws_route_table" "route_table" {
-  vpc_id = aws_vpc.demo_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-  tags = {
-    Name = "jirka-RT"
-  }
-}
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.demo_vpc.id
-  cidr_block        = "10.0.1.0/24" # Replace with your desired CIDR block
-  availability_zone = "eu-central-1a"
-  tags = {
-    Name = "jirka-subnet"
-  }
-}
-
-resource "aws_route_table_association" "route_table_asso" {
-  subnet_id      = aws_subnet.my_subnet.id
-  route_table_id = aws_route_table.route_table.id
-}
-
-resource "aws_security_group" "instance_sg" {
-  name        = "jirka_webserver_sg2"
-  description = "Allow inbound SSH and HTTP traffic"
-  vpc_id      = aws_vpc.demo_vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "jirka-web-traffic"
+data "aws_security_group" "existing" {
+  filter {
+    name   = "tag:Name"
+    values = ["jirka-web-traffic"]
   }
 }
 
@@ -98,7 +110,6 @@ data "aws_ami" "ecs_optimized" {
 }
 
 resource "aws_ecs_cluster" "cluster" {
-  count = length(data.aws_instances.existing.ids) > 0 ? 0 : 1
   name = "jirka-cluster-${var.subdomain}"
 
   tags = {
@@ -109,14 +120,12 @@ resource "aws_ecs_cluster" "cluster" {
 }
 
 resource "aws_instance" "webserver" {
-  count = length(data.aws_instances.existing.ids) > 0 ? 0 : 1
-
   ami               = data.aws_ami.ecs_optimized.id
   instance_type     = "t2.micro"
   availability_zone = "eu-central-1a"
   associate_public_ip_address = true
-  vpc_security_group_ids = [aws_security_group.instance_sg.id]
-  subnet_id              = aws_subnet.my_subnet.id
+  vpc_security_group_ids = [data.aws_security_group.existing.id]
+  subnet_id              = data.aws_subnet.existing.id
 
   tags = {
     Name = "jirka-web-server-instance-${var.subdomain}"
@@ -126,15 +135,15 @@ resource "aws_instance" "webserver" {
 
   user_data = <<-EOF
     #!/bin/bash
-    echo "ECS_CLUSTER=${aws_ecs_cluster.cluster[0].name}" >> /etc/ecs/ecs.config
+    echo "ECS_CLUSTER=${aws_ecs_cluster.cluster.name}" >> /etc/ecs/ecs.config
     sudo yum update -y
     sudo yum install httpd -y
     sudo yum install mod_ssl -y
     sudo systemctl start httpd
-    sudo bash -c 'echo Congratulations! This is your Appmixer Webserver Installation. HOSTNAME: $(hostname) > /var/www/html/index.html'
+    sudo bash -c 'echo Congratulations! This is your Webserver Installation. ID: ${var.subdomain} HOSTNAME: $(hostname) > /var/www/html/index.html'
   EOF
 }
 
 output "webserver_url" {
-  value = aws_instance.webserver[0].public_ip
+  value = aws_instance.webserver.public_ip
 }
